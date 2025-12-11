362875a4b1db1c5ae02d178dd9763700c6bd1858
Gabriel <gabrielpaulsrud@yahoo.com>
2025-12-11 20:57:42 +0100

    loops

---
+++ gem5/loops.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main() {
+    volatile uint64_t sum = 0;
+
+    for (uint64_t i = 0; i < 100000000ULL; i++) {
+        sum += (i & 1);
+    }
+
+    printf("sum = %llu\n", (unsigned long long)sum);
+    return 0;
+}
